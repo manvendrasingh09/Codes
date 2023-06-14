1ecb07eff6cc7b275510529551440eb6541cb1a6
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-06-14 20:06:04 +0530

    using VS code

--- Operating System/Semphore.cpp
+++ Operating System/Semphore.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
 #include <thread>
-#include <semaphore.h>
+#include <mutex>
+#include <condition_variable>
 
-sem_t semaphore;
+using namespace std;
 
-void criticalSection()
-{
-    sem_wait(&semaphore);
-    // Critical section code
-    std::cout << "Executing critical section..." << std::endl;
-    sem_post(&semaphore);
+class Semaphore {
+public:
+    Semaphore(int count) : count_(count) {}
+
+    void Wait() {
+        unique_lock<mutex> lock(mutex_);
+        while (count_ == 0) {
+            condition_.wait(lock);
+        }
+        count_--;
+    }
+
+    void Signal() {
+        unique_lock<mutex> lock(mutex_);
+        count_++;
+        condition_.notify_one();
+    }
+
+private:
+    int count_;
+    mutex mutex_;
+    condition_variable condition_;
+};
+
+Semaphore semaphore(1);
+
+void WorkerThread(int id) {
+    semaphore.Wait();
+
+    // Critical section
+    cout << "Thread " << id << " is in the critical section." << endl;
+
+    semaphore.Signal();
+
+    // Remaining section
+    cout << "Thread " << id << " is in the remaining section." << endl;
 }
 
-int main()
-{
-    sem_init(&semaphore, 0, 1);
-
-    std::thread t1(criticalSection);
-    std::thread t2(criticalSection);
+int main() {
+    thread t1(WorkerThread, 1);
+    thread t2(WorkerThread, 2);
 
     t1.join();
     t2.join();
-
-    sem_destroy(&semaphore);
 
     return 0;
 }
