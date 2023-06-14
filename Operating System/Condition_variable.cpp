cbb6bd2924fbdb3b3a91c25fc62e7e1832c63952
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-06-14 19:53:37 +0530

    using VS code

--- Operating System/Condition_variable.cpp
+++ Operating System/Condition_variable.cpp
@@ -3,23 +3,25 @@
 #include <mutex>
 #include <condition_variable>
 
-std::mutex mtx;
-std::condition_variable cv;
+using namespace std;
+
+mutex mtx;
+condition_variable cv;
 bool ready = false;
 
 void waitForSignal()
 {
-    std::unique_lock<std::mutex> lock(mtx);
+    unique_lock<mutex> lock(mtx);
     cv.wait(lock, [] { return ready; });
     // Critical section code
-    std::cout << "Executing critical section..." << std::endl;
+    cout << "Executing critical section..." << endl;
 }
 
 void sendSignal()
 {
-    std::this_thread::sleep_for(std::chrono::seconds(2)); // Simulate some work
+    this_thread::sleep_for(chrono::seconds(2)); // Simulate some work
     {
-        std::lock_guard<std::mutex> lock(mtx);
+        lock_guard<mutex> lock(mtx);
         ready = true;
     }
     cv.notify_one();
@@ -27,8 +29,8 @@ void sendSignal()
 
 int main()
 {
-    std::thread t1(waitForSignal);
-    std::thread t2(sendSignal);
+    thread t1(waitForSignal);
+    thread t2(sendSignal);
 
     t1.join();
     t2.join();
