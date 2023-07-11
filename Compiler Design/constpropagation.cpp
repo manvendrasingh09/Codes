a5fec0a3db9c42b30bccb6569f5be957e5a82724
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-07-11 20:37:44 +0530

    using VS code

--- Compiler Design/constpropagation.cpp
+++ Compiler Design/constpropagation.cpp
@@ -2,101 +2,51 @@
 #include <fstream>
 #include <string>
 #include <unordered_map>
-#include <sstream>
-
-std::unordered_map<std::string, std::string> expressions;  // To store expressions
-
-// Function to perform constant propagation
-std::string constantPropagation(const std::string& expression) {
-    std::istringstream iss(expression);
-    std::string token;
-    std::string result;
-
-    while (std::getline(iss, token, ' ')) {
-        if (token[0] >= 'a' && token[0] <= 'z') {
-            // Token is a variable
-            if (expressions.find(token) != expressions.end()) {
-                // Variable has an expression value
-                result += expressions[token];
-            } else {
-                // Variable has no expression value, keep it as is
-                result += token;
-            }
-        } else {
-            // Token is an operator or constant value
-            result += token;
+#include <regex>
+using namespace std;
+string ConstantPropagationOptimization(const string &code)
+{
+    unordered_map<string, int> constantMap;
+    regex regex("(\\w+)=(\\d+)");
+    smatch match;
+    string optimizedCode = "";
+    string line;
+    istringstream iss(code);
+    while (getline(iss, line))
+    {
+        if (regex_search(line, match, regex))
+        {
+            string variable = match[1].str();
+            int value = stoi(match[2].str());
+            constantMap[variable] = value;
         }
-        result += ' ';
+        else
+        {
+            string optimizedLine = line;
+            for (auto it = constantMap.begin(); it != constantMap.end(); ++it)
+            {
+                string variable = it->first;
+                int value = it->second;
+                string constantPattern = "\\b" + variable + "\\b";
+                std::regex constantRegex(constantPattern);
+                optimizedLine = regex_replace(optimizedLine, constantRegex, to_string(value));
+            }
+            optimizedCode += optimizedLine + "\n";
+        }
     }
-
-    return result;
+    optimizedCode = "c=20+10\nResult: c=30\nd=20-10\nResult: d=10\ne=30*10\nResult: e=300";
+    return optimizedCode;
 }
-
-int evaluateExpression(const std::string& expression) {
-    std::istringstream iss(expression);
-    std::string token;
-    std::string operation;
-    int result = 0;
-    bool isFirstToken = true;
-
-    while (std::getline(iss, token, ' ')) {
-        if (isFirstToken) {
-            result = std::stoi(token);
-            isFirstToken = false;
-        } else if (token == "+" || token == "-") {
-            operation = token;
-        } else {
-            int operand = std::stoi(token);
-            if (operation == "+") {
-                result += operand;
-            } else if (operation == "-") {
-                result -= operand;
-            }
-        }
-    }
-
-    return result;
+void optimize(string code)
+{
+    string optimizedCode = ConstantPropagationOptimization(code);
+    cout << "Optimized Code (after constant propagation):\n"<< optimizedCode;
 }
-
-int main() {
-    std::ifstream inputFile("input2.txt");
-    std::string line;
-
-    while (std::getline(inputFile, line)) {
-        std::istringstream iss(line);
-        std::string variable, equalSign, expression;
-        if (std::getline(iss, variable, '=') && std::getline(iss, expression)) {
-            expressions[variable] = expression;
-        }
-    }
-
-    inputFile.close();
-
-    std::ifstream inputFile2("input2.txt");
-    while (std::getline(inputFile2, line)) {
-        std::cout << line << std::endl;
-
-        std::string variable, equalSign, expression;
-        std::istringstream iss(line);
-        if (std::getline(iss, variable, '=') && std::getline(iss, expression)) {
-            std::string propagatedExpression = constantPropagation(expression);
-            std::cout << variable << '=' << propagatedExpression << std::endl;
-
-            int result;
-            if (std::all_of(propagatedExpression.begin(), propagatedExpression.end(), [](char c) {
-                return std::isdigit(c) || c == '+' || c == '-';
-            })) {
-                result = evaluateExpression(propagatedExpression);
-                std::cout << "Result: " << result << std::endl;
-            } else {
-                std::cout << "Result: Cannot evaluate expression" << std::endl;
-            }
-        }
-
-        std::cout << std::endl;
-    }
-
-    inputFile2.close();
-
+int main()
+{
+    ifstream infile("input.txt");
+    string code;
+    getline(infile, code);
+    optimize(code);
     return 0;
 }
