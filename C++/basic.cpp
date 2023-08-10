cf5c0d851950c78da87e28d4bb97ccdf6dfc3821
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-08-10 22:46:04 +0530

    using VS code

---
+++ C++/basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5,b=6;
+    char c='a';
+    float d=3.14;
+    cout<<"Hello World!"<<" \nHere the value of a is "<<a<<" and the value of b is "<<b<<".";
+    cout<<"\n"<<c;
+    cout<<"\n"<<d;
+    return 0;
+}
