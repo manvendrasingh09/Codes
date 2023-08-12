29c0da4f6df18ab82df381e38f27808f7b2be07c
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-08-12 17:36:24 +0530

    using VS code

--- C++/switchcase.cpp
+++ C++/switchcase.cpp
@@ -8,17 +8,17 @@ int main()
     switch (age)
     {
     case 18:
-        cout<<"Your age is 18";
+    cout<<"Your age is 18"<<endl;
         break; //to terminate the program if case matches with the input
     case 22:
-        cout<<"Your age is 22";
+        cout<<"Your age is 22"<<endl;
         break;
     case 30:
-        cout<<"Your age is 18";
+        cout<<"Your age is 18"<<endl;
         break;
     default:
-        cout<<"Enter a valid age";
+        cout<<"Enter a valid age"<<endl;
         break;
     }
-    cout<<"Done with Switch cases";
+    cout<<"Done with Switch cases"<<endl;
 }
