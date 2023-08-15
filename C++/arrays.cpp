643398addf2800db1f8c0d4f4727e631fc93c71f
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-08-15 20:43:21 +0530

    using VS code

---
+++ C++/arrays.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks[4]={98,89,87,80};
+    cout<<marks[0]<<endl;
+    marks[1]=99;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;   
+    return 0;
+}   
