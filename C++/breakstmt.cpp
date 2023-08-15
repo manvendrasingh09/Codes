d73d9801ad54b2666dee2318069d9c8cab89d97b
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-08-15 19:41:30 +0530

    using VS code

---
+++ C++/breakstmt.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=4;i++)
+    {
+        cout<<i<<endl;
+        if(i==2)
+        {
+            break;
+        }
+    }
+    return 0;
+}
