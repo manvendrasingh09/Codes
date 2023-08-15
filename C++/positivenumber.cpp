70cd157228a82726e0f200a1f36b53e9284e3083
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-08-15 22:29:30 +0530

    using VS code

---
+++ C++/positivenumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the Number: ";
+    while(n>=0)
+    {
+        cin>>n;
+    }
+    cout<<"Negative Number Entered";
+    return 0;
+}
