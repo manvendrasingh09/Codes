bbbeadd4dec181cebb866bb1f10c0fbef7096fbc
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-08-15 21:15:38 +0530

    using VS code

--- C++/arrays.cpp
+++ C++/arrays.cpp
@@ -4,9 +4,22 @@ int main()
 {
     int marks[4]={98,89,87,80};
     marks[1]=99;
+    cout<<"The values in arrays are: ";
+    cout<<"[";
     for(int i=0;i<4;i++)
     {
-        cout<<marks[i]<<endl;
+        cout<<marks[i]<<" ";
     }
+    cout<<"]"<<endl;
+    //Pointer and Array
+    int* p=marks; //In array no need to write &marks
+    cout<<"The address of marks[0] is: "<<p<<endl;
+    cout<<"The value of marks[0] is: "<<*p<<endl;
+    cout<<"The address of marks[1] is: "<<p+1<<endl;
+    cout<<"The value of marks[1] is: "<<*(p+1)<<endl;
+    cout<<"The address of marks[2] is: "<<p+2<<endl;
+    cout<<"The value of marks[2] is: "<<*(p+2)<<endl;
+    cout<<"The address of marks[3] is: "<<p+3<<endl;
+    cout<<"The value of marks[3] is: "<<*(p+3)<<endl;
     return 0;
 }   
