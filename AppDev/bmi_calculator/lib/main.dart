ffa1f2726110a07569926301efe5d2247255778d
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-09-07 19:53:38 +0530

    using Android Studio

--- AppDev/bmi_calculator/lib/main.dart
+++ AppDev/bmi_calculator/lib/main.dart
@@ -1,39 +1,21 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(const BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   const BMICalculator({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+        primaryColor: const Color(0xFF0A0E21),
+        appBarTheme: const AppBarTheme(
+          color: Color(0xFF0A0E21),
+        ),
       ),
-      body: const Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
+      home: const InputPage(),
     );
   }
 }
