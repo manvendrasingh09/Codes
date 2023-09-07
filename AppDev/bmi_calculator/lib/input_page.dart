4ba1a7bbf2eed37b04415aea392284dd47345737
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-09-08 01:20:42 +0530

    using Android Studio

--- AppDev/bmi_calculator/lib/input_page.dart
+++ AppDev/bmi_calculator/lib/input_page.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
+import 'constants.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const bottomContainerColour = Color(0xFFEB1555);
+enum Gender { none, male, female }
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -15,6 +14,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender = Gender.none;
+  int height = 130;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +25,22 @@ class _InputPageState extends State<InputPage> {
         ),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Expanded(
+          Expanded(
             child: Row(
               children: [
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColour,
-                    cardChild: IconContent(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    colour: selectedGender == Gender.male
+                        ? kActiveCardColour
+                        : kInActiveCardColour,
+                    cardChild: const IconContent(
                       icon: FontAwesomeIcons.mars,
                       label: 'MALE',
                     ),
@@ -39,8 +48,15 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColour,
-                    cardChild: IconContent(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    colour: selectedGender == Gender.female
+                        ? kActiveCardColour
+                        : kInActiveCardColour,
+                    cardChild: const IconContent(
                       icon: FontAwesomeIcons.venus,
                       label: 'FEMALE',
                     ),
@@ -49,27 +65,75 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          const Expanded(
-            child: ReusableCard(colour: activeCardColour, cardChild: Column()),
+          Expanded(
+            child: ReusableCard(
+              colour: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'HEIGHT',
+                    style: kLabelStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(height.toString(), style: kFontStyle),
+                      const Text(
+                        ' cm',
+                        style: kLabelStyle,
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      thumbShape:
+                          const RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          const RoundSliderOverlayShape(overlayRadius: 30.0),
+                      overlayColor: const Color(0x29eb1555),
+                      thumbColor: const Color(0xffeb1555),
+                      activeTrackColor: Colors.white,
+                      inactiveTrackColor: const Color(0xff8d8e98),
+                      trackHeight: 3.0,
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 60,
+                      max: 200,
+                      activeColor: const Color(0xFFEB1555),
+                      inactiveColor: const Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(
+                          () {
+                            height = newValue.round();
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           const Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(
-                      colour: activeCardColour, cardChild: Column()),
+                  child: ReusableCard(colour: kActiveCardColour),
                 ),
                 Expanded(
-                  child: ReusableCard(
-                      colour: activeCardColour, cardChild: Column()),
+                  child: ReusableCard(colour: kActiveCardColour),
                 ),
               ],
             ),
           ),
           Container(
-            color: bottomContainerColour,
+            color: kBottomContainerColour,
             margin: const EdgeInsets.only(top: 10.0),
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
             width: double.infinity,
           )
         ],
