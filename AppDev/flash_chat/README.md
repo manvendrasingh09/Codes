2f3b562a787e300415e7838e584423e2aae8e05b
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-09-14 16:22:13 +0530

    using Android Studio

---
+++ AppDev/flash_chat/README.md
@@ -0,0 +1,6 @@
+# Flash Chat
+
+This is my 12th Flutter project.
+
+## Previw of the Application
+
---
+++ AppDev/flash_chat/android/app/src/main/kotlin/manvendrasingh/com/flash_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package manvendrasingh.com.flash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
