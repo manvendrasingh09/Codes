1a9e0605606a37fdddd3c081172e5167ffcd6e12
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-09-18 00:28:34 +0530

    using Android Studio

---
+++ AppDev/todoey/lib/screens/task_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: const FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+              ),
+              child: const TasksList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TasksList extends StatelessWidget {
+  const TasksList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        ListTile(
+          title: Text('Task1 '),
+          trailing: Checkbox(
+            value: false,
+            onChanged: null,
+          ),
+        ),
+        ListTile(
+          title: Text('Task1 '),
+          trailing: Checkbox(
+            value: false,
+            onChanged: null,
+          ),
+        ),
+      ],
+    );
+  }
+}
