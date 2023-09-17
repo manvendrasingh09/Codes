4b32f0bb60614b8d025fbbb78cd1009b967741e6
manvendrasingh09 <m.s.jaunpur@gmail.com>
2023-09-17 17:43:24 +0530

    using Android Studio

--- AppDev/flash_chat/lib/screens/chat_screen.dart
+++ AppDev/flash_chat/lib/screens/chat_screen.dart
@@ -69,6 +69,38 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                List<MessageBubble> messageBubbles = [];
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+                final messages = snapshot.data?.docs;
+                for (var message in messages!) {
+                  final messageText = message.get('Text');
+                  final messageSender = message.get('Sender');
+                  //final messageTime = message.get('Time');//TODO:
+                  final messageBubble = MessageBubble(
+                    sender: messageSender,
+                    text: messageText, /*time: messageTime*/
+                  ); //TODO:
+                  messageBubbles.add(messageBubble);
+                }
+
+                return Expanded(
+                  child: ListView(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                    children: messageBubbles,
+                  ),
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -89,7 +121,7 @@ class _ChatScreenState extends State<ChatScreen> {
                       _firestore.collection('messages').add({
                         'Text': messageText,
                         'Sender': loggedInUser.email,
-                        'Time': FieldValue.serverTimestamp(),
+                        //'Time': FieldValue.serverTimestamp(),//TODO:
                       });
                     },
                     child: const Text(
@@ -102,6 +134,46 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble(
+      {required this.sender,
+      required this.text,
+      //required this.time,//TODO:
+      super.key});
+
+  final String sender;
+  final String text;
+  //final Timestamp time;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Text(sender),
+          Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(30.0),
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                text, //TODO:
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
