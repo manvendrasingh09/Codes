d209aabfeb4b37860b94f540276a0514696eb489
Manvendra Singh <m.s.jaunpur@gmail.com>
2024-01-01 17:02:38 +0530

    Day 1 of 100 Days of Python is Completed

---
+++ Python/day1project.py
@@ -0,0 +1,7 @@
+#Band Name Generator
+print("Welcome to Band Name Generator")
+print("What is your City Name: ")
+name=input()
+print("What is your Pet Name: ")
+pet=input()
+print("Your New Band Name is "+name+" "+pet)
