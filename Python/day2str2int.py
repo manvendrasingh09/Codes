76a84c89ccc1d10707070f8e1af8f5f87b49402d
Manvendra Singh <m.s.jaunpur@gmail.com>
2024-01-02 21:28:41 +0530

    Using VS Code

---
+++ Python/day2str2int.py
@@ -0,0 +1,6 @@
+#Numbers are in inputted as a string and we have to find the sum of digits
+two_digit_number = input()
+first=int(two_digit_number[0])
+second=int(two_digit_number[1])
+sum=first+second
+print(sum)
