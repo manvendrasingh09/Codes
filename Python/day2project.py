7aaf9635810f3bdb5bb0597469c0fea51196bbed
Manvendra Singh <m.s.jaunpur@gmail.com>
2024-01-02 23:40:53 +0530

    Day 2 of 100 Days of Python is Completed

---
+++ Python/day2project.py
@@ -0,0 +1,11 @@
+#Tip Calculator
+
+print("Welcome to Tip Calculator")
+billWithoutTip=float(input("What was the total bill? $"))
+tip=int(input("How much tip you want to give? $"))
+people=int(input("Number of people to Split bill with: "))
+tipPer=tip/100
+totalTip=billWithoutTip*tipPer
+totalbill=billWithoutTip+totalTip
+final=totalbill/people
+print("Each person will pay: $"+"{:.2f}".format(final))
