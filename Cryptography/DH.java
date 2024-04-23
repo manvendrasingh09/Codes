540bf84249b9526e0ab235331a8c20ca6669ab84
Manvendra Singh <m.s.jaunpur@gmail.com>
2024-04-23 14:32:45 +0530

    using VS code

--- Cryptography/DH.java
+++ Cryptography/DH.java
@@ -24,11 +24,11 @@ class DH {
     }
     public static void main(String[] args) {
         BigInteger p = new BigInteger("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
-                                      "29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
-                                      "EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
-                                      "E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
-                                      "EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381" +
-                                      "FFFFFFFFFFFFFFFF", 16);
+                                    "29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
+                                    "EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
+                                    "E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
+                                    "EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381" +
+                                    "FFFFFFFFFFFFFFFF", 16);
         BigInteger g = new BigInteger("2");
         DH alice = new DH(p, g);
         DH bob = new DH(p, g);
