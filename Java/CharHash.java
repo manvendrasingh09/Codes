9e8760f38964a93ff95774bdb6349ffd2e1f4770
Manvendra Singh <m.s.jaunpur@gmail.com>
2024-06-01 20:09:04 +0530

    using VS code

--- Java/CharHash.java
+++ Java/CharHash.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class CharHash {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the String: ");
         String s=sc.nextLine();
         int hash[]=new int[26];
 
         for(int i=0; i<s.length(); i++){
             hash[s.charAt(i)-'a']++;
         }
-
+        System.out.print("Enter the Number of Queries: ");
         int q=sc.nextInt();
         sc.nextLine();
         for(int i=1; i<=q; i++){
