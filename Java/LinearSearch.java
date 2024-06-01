d3b12d078b433586355b3cbac6b3b99f3293cbde
Manvendra Singh <m.s.jaunpur@gmail.com>
2024-06-01 13:20:00 +0530

    using VS code

---
+++ Java/LinearSearch.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Size of the Array: ");
+        int size=sc.nextInt();
+        int num[]=new int[size];
+        System.out.println("Enter the Value in the Array");
+        for(int i=0;i<size;i++){
+            num[i]=sc.nextInt();
+        }
+        System.out.print("Enter the Number to be found in the array: ");
+        int x=sc.nextInt();
+        boolean found=false;
+        for(int i=0;i<size;i++){
+            if(num[i]==x){
+                System.out.println("Number found at index:"+i);
+                found=true;
+            }
+        }
+        if (found==false) {
+            System.out.println("Number not present in array");
+        }
+        
+    }
+}
