200db9eece2d547247dcd9fb1b963159326dd0b8
Manvendra Singh <m.s.jaunpur@gmail.com>
2024-06-03 19:27:09 +0530

    using VS code

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -5,12 +5,17 @@ public class BubbleSort {
     public static void bubble(int arr[], int size){
         for(int i=size-1; i>=1; i--){
             int temp;
+            int didSwap=0;
             for(int j=0; j<=i-1; j++){
                 if(arr[j]>arr[j+1]){
                     temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    didSwap=1;
                 }
+            }
+            if(didSwap==0){
+                break;
             }
         }
     }
