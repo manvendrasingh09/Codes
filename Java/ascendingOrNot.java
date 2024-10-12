ed01dc595517dc14d48006c5e762ecc5c74312c5
Manvendra Singh <m.s.jaunpur@gmail.com>
2024-10-12 14:49:22 +0530

    using vs code

--- Java/ascendingOrNot.java
+++ Java/ascendingOrNot.java
@@ -1,28 +1,28 @@
 import java.util.*;
 public class ascendingOrNot {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter The Size of the Array: ");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
-        System.out.print("Enter The Elements of the Array: ");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
-        }
-        boolean isAscending=true;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter The Size of the Array: ");
+            int size = sc.nextInt();
+            int arr[] = new int[size];
+            System.out.print("Enter The Elements of the Array: ");
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+            boolean isAscending = true;
 
-            for(int i=0;i<size-1;i++){
-                if(arr[i]>arr[i+1]){
-                    isAscending=false;
+            for (int i = 0; i < size - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    isAscending = false;
                     break;
                 }
-                
+
             }
-        if (isAscending==true) {
-            System.out.println("Array is in Ascending Order");
-        }
-        else{
-            System.out.println("Array is not in Ascending Order");
+            if (isAscending == true) {
+                System.out.println("Array is in Ascending Order");
+            } else {
+                System.out.println("Array is not in Ascending Order");
+            }
         }
     }
 }
